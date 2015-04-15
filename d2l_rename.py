bdca9b5ad417399dce318e67b02f4cf49c1d7ddd
mdomarat <mdomarat@cs.umanitoba.ca>
2015-04-15 13:39:04 -0500

    fix
    missing space for [AP]M match

--- d2l_rename.py
+++ d2l_rename.py
@@ -1,7 +1,7 @@
 import os,sys,re
 
 def un_d2l (filename,ext):
- r = r"[0-9]*-[0-9]* - ([\w-]* )*[\w-]* - [\w-]* [0-9]{1,2}, [0-9]{4} [0-9]{3,4} ([AP]M)?- (.*."+ext+")"  
+ r = r"[0-9]*-[0-9]* - ([\w-]* )*[\w-]* - [\w-]* [0-9]{1,2}, [0-9]{4} [0-9]{3,4} ([AP]M )?- (.*."+ext+")"  
  x = re.match(r,filename)
  if x:
    return x.groups()[2]
